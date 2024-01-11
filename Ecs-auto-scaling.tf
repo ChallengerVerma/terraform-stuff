c4cabf799cdcc34c202f7ca5f75570704ee72fef
ChallengerVerma <er.prashantverma08@gmail.com>
2024-01-11 21:51:43 +0530

    Update Ecs-auto-scaling.tf
    added role and iam policy for ecs autoscaling.

--- Ecs-auto-scaling.tf
+++ Ecs-auto-scaling.tf
@@ -57,3 +57,66 @@ resource "aws_appautoscaling_scheduled_action" "scale_down" {
   }
   schedule            = "cron(0 18 * * ? *)"  # Scale down every day at 6 PM UTC, adjust as needed
 }
+
+resource "aws_iam_role" "ecs_autoscaling_role" {
+  name = "ecs-autoscaling-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      }
+    },
+    {
+      "Action": "sts:AssumeRole",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "application-autoscaling.amazonaws.com"
+      }
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "ecs_autoscaling_policy" {
+  name        = "ecs-autoscaling-policy"
+  description = "IAM policy for ECS Auto Scaling"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ecs:UpdateService",
+        "ecs:DescribeServices",
+        "ecs:ListTasks",
+        "ecs:DescribeTasks",
+        "application-autoscaling:RegisterScalableTarget",
+        "application-autoscaling:DeregisterScalableTarget",
+        "application-autoscaling:DescribeScalableTargets",
+        "application-autoscaling:DescribeScalingActivities",
+        "cloudwatch:PutMetricAlarm",
+        "cloudwatch:DescribeAlarms",
+        "cloudwatch:GetMetricStatistics",
+        "cloudwatch:DeleteAlarms"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_autoscaling_attachment" {
+  policy_arn = aws_iam_policy.ecs_autoscaling_policy.arn
+  role       = aws_iam_role.ecs_autoscaling_role.name
+}
+
